6dbf30099e93f3c558f0349db6226d799b1507b0
Aleksandar Sadzak <sadzak23@gmail.com>
2019-12-29 22:05:27 +0100

    Create BMI calculator

---
+++ src/components/Calculators/BMI/BmiCalculator.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { ValueUnit } from '../../common';
+
+export const BmiCalculator = ({ height, weight }) => {
+  const bmi = weight / Math.pow((height / 100), 2)
+  const bmiRound = Math.round(bmi * 10) / 10
+  return (
+    <View style={{ marginVertical: 20 }}>
+      {height && weight ? <ValueUnit value={bmiRound} unit='kg/cm2' /> : <Text>Please enter height & weight</Text>}
+    </View>
+  )
+};
