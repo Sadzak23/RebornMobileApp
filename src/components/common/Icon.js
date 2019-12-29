330c2503d1702267285112ed1fc9539751592d36
Aleksandar Sadzak <sadzak23@gmail.com>
2019-12-29 22:36:59 +0100

    Add Material Icons

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -3,12 +3,14 @@ import IconIon from 'react-native-vector-icons/Ionicons';
 import IconFa from 'react-native-vector-icons/FontAwesome';
 import IconFa5 from 'react-native-vector-icons/FontAwesome5';
 import IconMaterial from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconMa from 'react-native-vector-icons/MaterialIcons';
 
 export const Icon = ({ icon, type, size = 28, style, color='#fff' }) => {
   switch (type) {
     case 'ion': return <IconIon name={icon} size={size} color={color} style={style} />
     case 'fa': return <IconFa name={icon} size={size} color={color} style={style} />
     case 'fa5': return <IconFa5 name={icon} size={size} color={color} style={style} />
+    case 'mi': return <IconMa name={icon} size={size} color={color} style={style} />
     default: return <IconMaterial name={icon} size={size} color={color} style={style} />
   }
 };
