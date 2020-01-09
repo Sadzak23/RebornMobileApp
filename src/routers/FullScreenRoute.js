324a713c4ef7c294ed01450b5c0fbc87504484cd
Aleksandar Sadzak <sadzak23@gmail.com>
2020-01-10 00:17:26 +0100

    Hide Navigarion bar in Full Screen

--- src/routers/FullScreenRoute.js
+++ src/routers/FullScreenRoute.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-native';
+import { HideNavigationBar, ShowNavigationBar } from 'react-native-navigation-bar-color';
 
-export const FullScreenRoute = ({ path, exact, fullWidth, component: Component, ...rest }) => (
-  <Route
-    exact={exact}
-    path={path}
-    render={props => <Component {...props} {...rest} />}
-  />
-);
+export const FullScreenRoute = ({ path, exact, fullWidth, component: Component, ...rest }) => {
+  useEffect(() => {
+    HideNavigationBar();
+    return () => ShowNavigationBar();
+  }, [])
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={props => <Component {...props} {...rest} />}
+    />
+  )
+};
